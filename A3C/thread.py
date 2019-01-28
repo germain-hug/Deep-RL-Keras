1b7358227aa7a91df7bfa12e72658301df6457f3
germain-hug <hugo.germain@supelec.fr>
2019-01-28 14:11:11 +0100

    Added thread-safe variable updates and rendering

--- A3C/thread.py
+++ A3C/thread.py
@@ -2,13 +2,14 @@
 """
 
 import numpy as np
-
+from threading import Thread, Lock
 from keras.utils import to_categorical
 from utils.networks import tfSummary
 
 episode = 0
+lock = Lock()
 
-def training_thread(agent, Nmax, env, action_dim, f, summary_writer, tqdm, factor):
+def training_thread(agent, Nmax, env, action_dim, f, summary_writer, tqdm, render):
     """ Build threads to run shared computation across
     """
 
@@ -19,7 +20,9 @@ def training_thread(agent, Nmax, env, action_dim, f, summary_writer, tqdm, facto
         time, cumul_reward, done = 0, 0, False
         old_state = env.reset()
         actions, states, rewards = [], [], []
-        while not done:
+        while not done and episode < Nmax:
+            if render:
+                with lock: env.render()
             # Actor picks an action (following the policy)
             a = agent.policy_action(np.expand_dims(old_state, axis=0))
             # Retrieve new state, reward, and whether the state is terminal
@@ -34,14 +37,18 @@ def training_thread(agent, Nmax, env, action_dim, f, summary_writer, tqdm, facto
             time += 1
             # Asynchronous training
             if(time%f==0 or done):
+                lock.acquire()
                 agent.train_models(states, actions, rewards, done)
+                lock.release()
                 actions, states, rewards = [], [], []
 
         # Export results for Tensorboard
         score = tfSummary('score', cumul_reward)
         summary_writer.add_summary(score, global_step=episode)
         summary_writer.flush()
-        #
-        tqdm.set_description("Score: " + str(cumul_reward))
-        tqdm.update(int(episode * factor))
-        episode += 1
+        # Update episode count
+        with lock:
+            tqdm.set_description("Score: " + str(cumul_reward))
+            tqdm.update(1)
+            if(episode < Nmax):
+                episode += 1
